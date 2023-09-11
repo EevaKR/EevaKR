9cc79ffefee33266e16b43a1ce382f4d06f24b5c
eeva <t2roee00@students.oamk.fi>
2023-09-11 11:21:22 +0300

    11.9.matskua
    Sivuja päivitetty

--- THL/assets/scripts/script.js
+++ THL/assets/scripts/script.js
@@ -1,27 +1,56 @@
-const numbers2 = [1, 2, 3, 4, 5,];
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "Kyselyyn vastattu!";
+}
 
-<script type="text/javascript">
-var luku1 = 15;
-var luku2 = 15;
-var tulos = "Ei tulosta";
-var teksti = "";
+type="text/javascript"> 
+alert("Hello World"); 
 
-// Kirjoitetaan muuttujien arvot ennen muutoksia:
-teksti = "Ennen laskua: luku1 = "+luku1+", luku2 = "+luku2+" ja tulos = "+tulos;
-
-// Lisätään muuttujan arvon perään rivinvaihtoja (huomaa "+="-merkintä!):
-teksti += "<br /><br />"; 
-
-// Suoritetaan kertolasku, jonka vastaus tulee tulos-muuttujan arvoksi:
-tulos = luku1*luku2;
-
-// Kirjoitetaan uudet muuttujien arvot taas teksti-muuttujaan:
-teksti += "Laskun jälkeen: luku1 = "+luku1+", luku2 = "+luku2+" ja tulos = "+tulos;
-
-document.writeln(teksti);
-</script>
-
-new Date();
+function myFunction3() {
+    document.getElementById("demo").innerHTML = "Kyselyyn vastattu!";
+  }
 
 
-aikaNyt.getHours();
+
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "Testattu";
+}
+
+
+type="text/javascript"
+    var teksti = "Kiitos käynnistä!";
+    document.write(teksti); 
+
+
+type="text/javascript">  
+function msg(){  
+ alert("Hello Javatpoint");  
+}  
+
+function addDays(date, days) {
+    date.setDate(date.getDate() + days);
+    return date;
+  }
+  
+  //const date = new Date('2023-09-11T00:00:00.000Z');
+  //const newDate = addDays(date, 5);
+  //console.log(newDate);
+
+import { addDays } from 'date-fns';
+const date = new Date('2022-05-15T00:00:00.000Z');
+const newDate = addDays(date, 5);
+console.log(newDate); // 2022-05-20T00:00:00.000Z
+// original not modified
+console.log(date); // 2022-05-15T00:00:00.000Z
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleString();
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (("0"+dt.getDate()).slice(-2)) +"/"+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()+1).slice(-2)) +":"+ (("0"+dt.getMinutes()+1).slice(-2));
+
+
+
+
+
+
+  
